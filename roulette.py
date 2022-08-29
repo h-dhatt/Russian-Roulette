94086c0e2296b473ed64f5b86a14d53f1be4b7b6
h-dhatt <111666380+h-dhatt@users.noreply.github.com>
2022-08-29 00:55:23 -0400

    Create roulette.py

---
+++ roulette.py
@@ -0,0 +1,86 @@
+from random import randrange
+import time
+
+
+def main():
+
+    numOfRounds = 6
+    gun = [0, 0, 0, 0, 0, 0]
+    bullet = randrange(0, 6)
+    gun[bullet] = 1
+    player = False  
+    pc = False  
+
+    # menu
+    print("/********************************/")
+    print("    Welcome to russian roulette")
+    print("/********************************/")
+    time.sleep(2)
+    print("You are going to play against the pc")
+    time.sleep(2)
+    print("There is one gun and one bullet")
+    time.sleep(2)
+
+    # take input from the user
+    answer = input(
+        "Please press 'm' if you want to start first or 'p' if you want the pc to start first: "
+    )
+
+    # check input
+    while answer != "m" and answer != "p":
+        answer = input("please enter again ('m' or 'p'): ")
+
+    # set turn
+    if answer == 'm':
+        turn = "player"
+    else:
+        turn = "pc"
+
+    # game starts
+    while numOfRounds != 0 and (pc == False and player == False):
+        print(f"\nRound number {numOfRounds}/6")
+        time.sleep(1)
+        print("the gun is being loaded")
+        time.sleep(3)
+        print("the gun is placed on " + ("your head" if turn ==
+              "player" else "the cpu of the pc"))
+        time.sleep(3)
+        print("and...")
+        time.sleep(1)
+        print("...")
+        time.sleep(2)
+        print("...")
+        time.sleep(2)
+        print("...")
+        time.sleep(2)
+
+        # get the bullet in the chamber
+        shot = gun.pop(numOfRounds - 1)
+
+        if shot:
+            print("The gun went off!")
+            print("You died." if turn == "player" else "The PC died.")
+            if turn == "player":  # set up who died
+                player = True
+            else:
+                pc = True
+        else:
+            print("Nothing happened. Phew!")
+            if turn == "player":  # flip the turn
+                turn = "pc"
+            else:
+                turn = "player"
+
+        time.sleep(2)
+        numOfRounds -= 1
+
+    time.sleep(1)
+    print("")
+    if player:
+        print("You died! Better luck next time...")
+    else:
+        print("You survived!")
+    print("Goodbye")
+
+
+main()
